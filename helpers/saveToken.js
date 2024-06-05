d36a8cb0c115afb3300b97d520c72b4e456fdffa
chiennh <chiennh@avadagroup.com>
2024-06-05 17:57:24 +0700

    init model

--- helpers/saveToken.js
+++ helpers/saveToken.js
@@ -1,13 +1,13 @@
 module.exports = (res, accToken, refToken) => {
   res.cookie("accessToken", accToken, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 5 * 60 * 1000
+      Date.now() +  5 * 60 * 1000
     ),
     // secure:true,
   });
   res.cookie("refreshToken", refToken, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 7 * 24 * 60 * 60 * 1000
+      Date.now() + 7 * 24 * 60 * 60 * 1000
     ),
     // secure:true,
   });
